4497f7510626b810a680503ae901707fc453e191
Oliver Song <80020297+obs4qqf@users.noreply.github.com>
2021-06-29 17:50:47 -0400

    Add check if board full and reformat variable names

--- newDrawing.py
+++ newDrawing.py
@@ -1,17 +1,17 @@
 
 spaces = {0: [" ", " ", " "], 1: [" ", " ", " "], 2: [" ", " ", " "]}
-playerWin = False
-botWin = False
-gameOn = True
-playerTurn = True
+player_win = False
+bot_win = False
+game_on = True
+player_turn = True
 
 print('Welcome to the Tic Tac Toe Match')
 print('Beat the tic tac toe bot to win!')
 
 def check_winner():
-    global playerWin
-    global botWin
-    global gameOn
+    global player_win
+    global bot_win
+    global game_on
     matches = [spaces[0][0] == spaces[0][1] == spaces[0][2] != " ",
                spaces[1][0] == spaces[1][1] == spaces[1][2] != " ",
                spaces[2][0] == spaces[2][1] == spaces[2][2] != " ",
@@ -22,34 +22,45 @@ def check_winner():
                spaces[0][2] == spaces[1][1] == spaces[2][0] != " "]
     for match in matches:
         if match:
-            if not playerTurn:
-                playerWin = True
+            if not player_turn:
+                player_win = True
             else:
-                botWin = True
-            gameOn = False
+                bot_win = True
+            game_on = False
 
-while gameOn and not playerWin and not botWin:
+def check_board_full():
+    board_full = True
+    for row in spaces:
+        for col in spaces:
+            if spaces[row][col] == " ":
+                board_full = False
+    return board_full
+
+
+while game_on and not player_win and not bot_win:
     print('Tic Tac Toe Board:')
     print(f'|{spaces[0][0]}|{spaces[0][1]}|{spaces[0][2]}|\n|{spaces[1][0]}|{spaces[1][1]}|{spaces[1][2]}|\n|{spaces[2][0]}|{spaces[2][1]}|{spaces[2][2]}|')
-    if playerTurn:
-        rowNum = int(input('Pick a row to place a marker: '))
-        colNum = int(input('Pick a column to place a marker: '))
-        spaces[rowNum - 1][colNum - 1] = "O"
-        playerTurn = False
+    if player_turn:
+        row_num = int(input('Pick a row to place a marker: '))
+        col_num = int(input('Pick a column to place a marker: '))
+        spaces[row_num - 1][col_num - 1] = "O"
+        player_turn = False
     else:
-        rowNum = int(input('Pick a row to place a marker: '))
-        colNum = int(input('Pick a column to place a marker: '))
-        spaces[rowNum - 1][colNum - 1] = "X"
-        playerTurn = True
+        row_num = int(input('Pick a row to place a marker: '))
+        col_num = int(input('Pick a column to place a marker: '))
+        spaces[row_num - 1][col_num - 1] = "X"
+        player_turn = True
     check_winner()
+    if check_board_full():
+        game_on = False
 
-if not gameOn and not playerWin and not botWin:
+if not game_on and not player_win and not bot_win:
     print('Game was a tie')
 
-if not gameOn and playerWin and not botWin:
+if not game_on and player_win and not bot_win:
     print('Player has won')
 
-if not gameOn and not playerWin and botWin:
+if not game_on and not player_win and bot_win:
     print('Bot has won')
 
 
